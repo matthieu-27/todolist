55ed472aaab8a0ec2e1cfe5c491d0455a728c268
matthieu-27 <matthieu27@pm.me>
2026-04-07 09:46:49 +0200

    fix(controller): controller now returns thymeleaf page

--- src/main/java/fr/fms/todolist/controller/TaskController.java
+++ src/main/java/fr/fms/todolist/controller/TaskController.java
@@ -1,30 +1,29 @@
 package fr.fms.todolist.controller;
 
-import java.util.List;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
 public class TaskController {
+
     @GetMapping("/products")
-    public List<String> getProducts() {
-        return List.of("Laptop", "Mobile", "Tablet");
+    public String getProducts() {
+        return "tasks";
     }
 
     @GetMapping("/admin")
     public String adminOnly() {
-        return "Welcome Admin! You have full access.";
+        return "index";
     }
 
     @GetMapping("/")
     public RedirectView root() {
-        return new RedirectView("/index");
+        return new RedirectView("/home");
     }
 
-    @GetMapping("/index")
+    @GetMapping("/home")
     public String index() {
-        return "Welcome to index.";
+        return "tasks";
     }
 }
