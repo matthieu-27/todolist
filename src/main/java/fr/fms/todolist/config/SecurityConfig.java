08cbaeb2a1c450a33c4b5c27499c47b1c08d53eb
matthieu-27 <matthieu27@pm.me>
2026-04-08 13:18:47 +0200

    chore(config): add spring security methods

--- src/main/java/fr/fms/todolist/config/SecurityConfig.java
+++ src/main/java/fr/fms/todolist/config/SecurityConfig.java
@@ -4,25 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import fr.fms.todolist.dao.UserRepository;
+import fr.fms.todolist.entities.User;
 
 @Configuration
 public class SecurityConfig {
 
+    private final UserRepository userRepository;
+
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     // Define users
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("john")
-                .password("password123")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            if (user == null) {
+                throw new UsernameNotFoundException("User not found with username: " + username);
+            }
+            return user;
+        };
     }
 
     // Define security rules
@@ -39,5 +48,10 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
