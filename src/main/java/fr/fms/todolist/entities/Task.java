3859192f7812a78eea64f99acaa8941e0535c834
matthieu-27 <matthieu27@pm.me>
2026-04-08 09:03:32 +0200

    fix(task): remove unused imports

--- src/main/java/fr/fms/todolist/entities/Task.java
+++ src/main/java/fr/fms/todolist/entities/Task.java
@@ -15,7 +15,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
