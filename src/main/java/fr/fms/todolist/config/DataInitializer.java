ad6ae46b3ca0266d211b3124b87abe90c3efca9a
matthieu-27 <matthieu27@pm.me>
2026-04-08 15:30:32 +0200

    chore(data): add default todolist generation

--- src/main/java/fr/fms/todolist/config/DataInitializer.java
+++ src/main/java/fr/fms/todolist/config/DataInitializer.java
@@ -1,18 +1,23 @@
 package fr.fms.todolist.config;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import fr.fms.todolist.dao.TodoListRepository;
 import fr.fms.todolist.dao.UserRepository;
+import fr.fms.todolist.entities.TodoList;
 import fr.fms.todolist.entities.User;
 
 @Configuration
 public class DataInitializer {
 
     @Bean
-    public CommandLineRunner initData(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public CommandLineRunner initData(UserRepository userRepository, PasswordEncoder passwordEncoder,
+            TodoListRepository todoListRepository) {
         return args -> {
             // Vérifier si un utilisateur existe déjà
             if (userRepository.count() == 0) {
@@ -23,7 +28,18 @@ public class DataInitializer {
                         .email("john@example.com")
                         .build();
                 userRepository.save(defaultUser);
+
+                // Créer une TodoList par défaut pour l'utilisateur
+                TodoList defaultTodoList = TodoList.builder()
+                        .title("Ma Liste de Tâches")
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .user(defaultUser)
+                        .build();
+                todoListRepository.save(defaultTodoList);
+
                 System.out.println("Utilisateur par défaut créé : " + defaultUser.getUsername());
+                System.out.println("TodoList par défaut créée : " + defaultTodoList.getTitle());
             }
         };
     }
